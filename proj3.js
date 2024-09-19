dcd147f99352b68af281c021f76bdbf1c7ea1ffc
Abster24 <absny24@gmail.com>
2024-09-19 13:43:04 -0400

    Add files via upload

---
+++ proj3.js
@@ -0,0 +1,84 @@
+let cap = 0;
+document.getElementById("current").innerHTML = "Current Capacity: " + cap;
+
+// Adds the the current total capacity
+function addCapacity() {
+    let more = parseFloat(document.getElementById("amount").value);
+    if (isNaN(more)) {
+        document.getElementById("current").innerHTML = "Current Capacity: " + cap;
+        more = 0;
+    }
+    else {
+    cap = cap + more;
+    document.getElementById("current").innerHTML = "! Current Capacity: " + cap;
+    }
+}
+
+// displays a welcome message
+function displayName() {
+    let fname = document.getElementById("fname").value;
+    if (fname=="") {
+        alert("Name must be filled out");
+    }
+    else {
+    document.getElementById("displayName").innerHTML = "Welcome, " + fname + "!";
+    }
+}
+
+// if the user selects that they can give rides, display a hidden part of the form
+function displayDriverForm() {
+    document.getElementById("capacity").style.display = "block";
+}
+
+// if the user selects that they need a ride, display a hidden part of the form
+function displayRiderForm() {
+    const drivers = ['Greta', 'Mathew', 'Grant']
+    document.getElementById("select").style.display = "block";
+    let select = document.getElementById("drivers");
+    let listLength = document.getElementById("drivers");
+    if (listLength.options.length > 0) {
+        return;
+    } else {
+        for (let i = 0; i < drivers.length; i++) {
+            let optn = drivers[i];
+            let el = document.createElement("option");
+            el.textContent = optn;
+            el.value = optn;
+            select.appendChild(el);
+        }
+    }
+}
+
+// after hitting the submit button, "reserve" a spot
+function reserveSpot() {
+    let choice = document.getElementById("drivers").value;
+    let fname = document.getElementById("fname").value;
+    if (document.getElementById("need").checked) {
+        if (fname=="") {
+            alert("Name must be filled out");
+        }
+    switch (choice) {
+        case "Greta":
+            document.getElementById("displayName").innerHTML = "Welcome, " + fname + "! You have reserved a spot with Greta.";
+            break;
+        case "Mathew":
+            document.getElementById("displayName").innerHTML = "Welcome, " + fname + "! You have reserved a spot with Mathew.";
+            break;
+        case "Grant":
+            document.getElementById("displayName").innerHTML = "Welcome, " + fname + "! You have reserved a spot with Grant.";
+    }
+} else {
+    displayName();
+}
+}
+
+// once the form is completed, reset the non-input parts of the form
+function removeForm() {
+    document.getElementById("capacity").style.display = "none";
+    document.getElementById("select").style.display = "none";
+    document.getElementById("displayName").innterHTML = "";
+    document.getElementById("current").innerHTML = "Current Capacity: " + cap;
+}
+function resetForm() {
+    document.getElementById("myForm").reset();
+}
