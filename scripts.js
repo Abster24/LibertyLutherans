6de53e45a24a96d6759fa17049e2a6db052fa3a8
Abster24 <absny24@gmail.com>
2024-09-19 13:22:17 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let cap = 0;
 document.getElementById("current").innerHTML = "Current Capacity: " + cap;
 
+
 function addCapacity() {
     let more = parseFloat(document.getElementById("amount").value);
     if (isNaN(more)) {
@@ -22,11 +23,50 @@ function displayName() {
     }
 }
 
-function displayForm() {
+function displayDriverForm() {
     document.getElementById("capacity").style.display = "block";
 }
+
+function displayRiderForm() {
+    const drivers = ['Greta', 'Mathew', 'Grant']
+    document.getElementById("select").style.display = "block";
+    let select = document.getElementById("drivers");
+    let listLength = document.getElementById("drivers");
+    if (listLength.options.length > 0) {
+        return;
+    } else {
+        for (let i = 0; i < drivers.length; i++) {
+            let optn = drivers[i];
+            let el = document.createElement("option");
+            el.textContent = optn;
+            el.value = optn;
+            select.appendChild(el);
+        }
+    }
+}
+
+function reserveSpot() {
+    let choice = document.getElementById("drivers").value;
+    let fname = document.getElementById("fname").value;
+    if (fname=="") {
+        alert("Name must be filled out");
+    }
+    switch (choice) {
+        case "Greta":
+            document.getElementById("displayName").innerHTML = "Welcome, " + fname + "! You have reserved a spot with Greta.";
+            break;
+        case "Mathew":
+            document.getElementById("displayName").innerHTML = "Welcome, " + fname + "! You have reserved a spot with Mathew.";
+            break;
+        case "Grant":
+            document.getElementById("displayName").innerHTML = "Welcome, " + fname + "! You have reserved a spot with Grant.";
+    }
+}
+
 function removeForm() {
     document.getElementById("capacity").style.display = "none";
+    document.getElementById("select").style.display = "none";
+    document.getElementById("displayName").innterHTML = "";
     document.getElementById("current").innerHTML = "Current Capacity: " + cap;
 }
 function resetForm() {
